be5a372a19e0b86bd19580d708faf28a9a5a203f
Медведев Егор Евгеньевич <MedvedevEE@dom201.local>
2026-09-07 11:23:25 +0700

    Third Commit

--- main.py
+++ main.py
@@ -3,5 +3,5 @@
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('HELLO WORLD')
-
+    print('hi')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
